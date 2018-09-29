50fac0661a570675f1ba8784a2860ec7ab2b18dd
Daniel Santos <dfsantosbu@unal.edu.co>
2018-09-29 10:18:45 -0400

    Change some files of folder.

--- algorithms/NaiveFind.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-  string needle = "CD", haystack ="MANICD";
-  if(haystack.find(needle) != string::npos) cout << "Gotcha!!!";
-  else cout << "Not Gotcha";
-  cout << endl;
-  return 0;
-}
