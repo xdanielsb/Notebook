201352fab3947be22af3f72083e1efe0ffc1542f
Daniel Santos <dfsantosbu@unal.edu.co>
2018-10-10 14:48:52 -0400

    Update z algorithm.

--- algorithms/Zalgorithm.cpp
+++ algorithms/Zalgorithm.cpp
@@ -5,24 +5,33 @@ typedef vector<int> vi;
 
 //Complexity: O(N + M)
 vi z_val(string s){
-  int n = s.size();
+  int n = s.size(), L =0, R=0;
   vi z(n);
-  for( int  i = 1, l= 0, r=0; i< n; i++){
-    if( i <=r) 
-      z[i] = min( r-i+1, z[i-l]);
-    while( i+z[i] <n && s[ z[i] ] == s[i +z[i]])
-      z[i]++;
-    if( i+ z[i] -1 > r) 
-      l = i, r = i+z[i] -1;
+  for( int i = 1; i < n ; i++){
+    if( i > R){ // not prefix-substr
+      L = R = i;
+      while( R < n && s[R-L] == s[R]) R++;
+      z[i] = R - L; R--;
+    }else {
+      int k = i - L;
+      //there is no longer prefix start at s[i]
+      if( z[k] < R-i+1){
+        z[i] = z[k];
+      }else{
+        L = i;
+        while( R < n && s[R-L] == s[R]) R++;
+        z[i] = R-L; R--;
+      }
+    }
   }
   return z;
 }
 
 int main() {
-  string haystack = "abcabc", needle = "abc";
+  string haystack = "abcabca", needle = "abc";
   int n = haystack.size(), m = needle.size();
   vi z = z_val(needle + "#" + haystack);
-  for( int i = 0; i < n+m; i ++)
+  for( int i = 0; i < z.size(); i ++)
     cout << i << " "<< z[i] <<endl;
   return 0;
 }
