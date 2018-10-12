90d1f8a8ba7a398e0a46a4a3098bbe805520f4b2
Daniel Santos <dfsantosbu@unal.edu.co>
2018-10-11 23:16:27 -0400

    Add binomial for exercises.

--- mathematics/Binomial.cpp
+++ mathematics/Binomial.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 const int MAXN = 66;
 unsigned long long ch[MAXN+5][MAXN+5];
+
+int Cnk( ll n, ll k){
+  ll res =1;
+  // since C(n,k) == C(n, n-k)
+  if( k > n-k) k = n-k;
+  for( ll i = 0; i< k; i++){
+    res = res*1LL*(n-i);
+    res /= (i+1);
+  }
+  return res;
+}
 void binomial(int N){
   for (int n = 0; n <= N; ++n)
     ch[n][0] = ch[n][n] = 1;
