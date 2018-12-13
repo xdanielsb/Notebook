da60163d03bf534bf69f2d40b8449d354832c3ca
Daniel Santos <dfsantosbu@unal.edu.co>
2018-12-12 19:02:24 -0500

    Implement new version of LIS.

--- strings/LIS.cpp
+++ strings/LIS.cpp
@@ -1,31 +1,35 @@
 #include<bits/stdc++.h>
-#define debug(x) cout <<#x << " = " << x << endl
 using namespace std;
-
-typedef vector < int > vi;
-typedef pair < int , int > ii;
-
-
-void lis( vi & arr){
-  int l = arr.size();
-  vi m(l, 1);
-  for( int i = 1 ; i < l; i++){
-    for( int j = 0 ; j < i ; j++){
-      if( arr[j] < arr[i])
-      m[i] = max( m[i], m[j]+1);
+/*
+ * Complexity Nlog(N) 
+ */
+vector< int >  getLis( const vector < int > A){
+  int n = A.size();
+  if( n == 0) return {};
+  vector < int > tail ( n, 0);
+  vector < int > lis ( n, 1);
+  int ans = 1;
+  tail[0] = A[0];
+  for( int i = 1; i < n ; i++){
+    if( A[i] < tail[0] ) {
+      tail[0] = A[i];
+      lis[i] = 1;
+    }else if( A[i] > tail[ ans - 1] ) {
+      tail[ ans++ ] = A[i];
+      lis[ i ] = ans;
+    }else{
+      int cp = upper_bound( tail.begin(), 
+                            tail.begin()+ans, A[i]) - tail.begin();
+      tail[ cp ] = A[i];
+      lis[ i] = cp+1;
     }
   }
-  for( int &i : m) cout << i << " ";
-  cout <<endl;
+  return lis;
 }
+
 int main(){
-#ifdef LOCAL 
-  freopen("in", "r", stdin);
-#endif
-
   vector < int > A = { 1, 3, 32 ,2 ,78, 9,2};
-
-  lis ( A );
-
+  getLis( A );
+  // 1 2 3  2 4  3 3
   return 0;
 }
