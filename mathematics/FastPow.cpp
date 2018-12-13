8cca431f61a9f67e14b7f2285e7484a76378cb0b
Daniel Santos <dfsantosbu@unal.edu.co>
2018-12-12 19:02:42 -0500

    Change version Fast pow.

--- mathematics/FastPow.cpp
+++ mathematics/FastPow.cpp
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll modular_pow(ll  base, int exponent, ll  modulus){
- ll result = 1;
- while (exponent > 0){
- /* if y is odd, multiply base with result */
-		if (exponent & 1)
-			result = (result * base) % modulus;
-		/* exponent = exponent/2 */
-		exponent = exponent >> 1;
-		/* base = base * base */
-		base = (base * base) % modulus;
-	}
-	return result;
+inline ll add ( ll x, ll y){
+  return (x%MOD +y%MOD)%MOD;
 }
-int main(){
-	ll exp = 1023;
-	cout << modular_pow (2, exp, 999) << endl;
+inline ll mul( ll x , ll y){
+  return (x%MOD*1LL*y%MOD)%MOD;
+}
+
+inline ll fpow( ll x, ll p){ // (x^p)%MOD
+  ll res=1LL;
+  while( p ){
+    if( p & 1){
+      res = mul(res,x);
+    }
+    p >>= 1LL;
+    x = mul(x,x);
+  }
+  return res;
 }
